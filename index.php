f5caf29e2337bfa52efb27da5b2b8afd3b1256f8
BRAVET <antobravet@gmail.com>
2025-03-14 14:40:15 +0100

    ajout du syle index.css + test

--- index.php
+++ index.php
@@ -1,27 +1,53 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Le Ciel Burgouzz</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Burgouzz</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-<div>
-    <h1>Le Ciel Burgouzz</h1>
-    <a href="#">Connexion</a> <!-- Ajout d'un href pour rendre le lien cliquable -->
-</div>
-<div>
-    <h2>Menu</h2>
-    <div class="burger"> <!-- Ajout des guillemets pour la classe CSS -->
-        <?php
-        for ($i = 0; $i < 10; $i++) {
-            echo "yoyo ";
-        }
-        ?>
-        <button>Choisir</button>
+
+    <h1>Menu Burgouzz</h1>
+
+    <div class="menu">
+        <div class="menu-item">
+            <img src="Classic Burgouzz.png" alt="Classic Burgouzz">
+            <h2>Classic Burgouzz</h2>
+            <p>Smash burger de viande française et cheddar mature de Somerset, sauce burgouzz avec salade, cornichons et pain bun brioché.</p>
+        </div>
+
+        <div class="menu-item">
+            <img src="Green Burgouzz.png" alt="Green Burgouzz">
+            <h2>Green Burgouzz</h2>
+            <p>Un steak d’aubergine mariné et cheddar mature de Somerset, spicy mayo, salade, kimchi dans un pain brioché.</p>
+        </div>
+
+        <div class="menu-item">
+            <img src="Kaarage Burgouzz.png" alt="Kaarage Burgouzz">
+            <h2>Kaarage Burgouzz</h2>
+            <p>Poulet kaarage mariné et cheddar mature de Somerset avec une sauce mayonnaise sriracha, salade, kimchi et bun brioché.</p>
+        </div>
+
+        <div class="menu-item">
+            <img src="Summer Burgouzz.png" alt="Summer Burgouzz">
+            <h2>Summer Burgouzz</h2>
+            <p>Un steak smashé de viande française et cheddar mature de Somerset, guacamole au kiwi, salade, oignons rouges et bun brioché.</p>
+        </div>
+
+        <div class="menu-item">
+            <img src="Truffle Burgouzz.png" alt="Truffle Burgouzz">
+            <h2>Truffle Burgouzz</h2>
+            <p>Smash burger de viande française, cheddar mature, tapenade de truffes noires du Périgord et bun brioché.</p>
+        </div>
+
+        <div class="menu-item">
+            <img src="Winter Burgouzz.png" alt="Winter Burgouzz">
+            <h2>Winter Burgouzz</h2>
+            <p>Deux steaks smashés, fromage à raclette fumé, mayonnaise miel et moutarde, salade, compotée d’oignons et de marrons, bun bacon de dinde et épices de Noël.</p>
+        </div>
     </div>
-</div>
-    
+
 </body>
 </html>
+
