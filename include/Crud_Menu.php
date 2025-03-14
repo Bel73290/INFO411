217c3ee3262653360daa6af704c62df14ccf67c6
Ilias Bellir <bellirilias@gmail.com>
2025-03-14 15:06:25 +0100

    debug

--- include/Crud_Menu.php
+++ include/Crud_Menu.php
@@ -9,28 +9,28 @@ function insert_menu($CONNEXION, $nom, $description, $prix) {
     $sql = "INSERT INTO menu (nom, description, prix) VALUES ('$nom', '$description', '$prix')";
     global $debeug;
     if ($debeug) echo $sql;
-    return mysqli_query($conn, $sql);
+    return mysqli_query($CONNEXION, $sql);
 }
 
 function update_menu($CONNEXION, $id, $nom, $description, $prix) {
     $sql = "UPDATE menu SET nom='$nom', description='$description', prix='$prix' WHERE id = $id";
     global $debeug;
     if ($debeug) echo $sql;
-    return mysqli_query($conn, $sql);
+    return mysqli_query($CONNEXION, $sql);
 }
 
 function delete_menu($CONNEXION, $id) {
     $sql = "DELETE FROM menu WHERE id = $id";
     global $debeug;
     if ($debeug) echo $sql;
-    return mysqli_query($conn, $sql);
+    return mysqli_query($CONNEXION, $sql);
 }
 
 function select_menu($CONNEXION, $id) {
     $sql = "SELECT * FROM menu WHERE id = $id";
     global $debeug;
     if ($debeug) echo $sql;
-    $res = mysqli_query($conn, $sql);
+    $res = mysqli_query($CONNEXION, $sql);
     return mysqli_fetch_assoc($res);
 }
 
@@ -38,7 +38,7 @@ function list_menus($CONNEXION) {
     $sql = "SELECT * FROM menu";
     global $debeug;
     if ($debeug) echo $sql;
-    $res = mysqli_query($conn, $sql);
+    $res = mysqli_query($CONNEXION, $sql);
     return rs_to_tab($res);
 }
 
