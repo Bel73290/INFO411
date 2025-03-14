1eb275f6e40206773f8c8bf07fcca9d61c2a94a1
Ilias Bellir <bellirilias@gmail.com>
2025-03-14 13:56:22 +0100

    test

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Le Ciel burgouzz</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+<div>
+    <h1>Le Ciel Burgouzz</h1>
+    <a>Connexion</a>
+</div>
+<div>
+    <div class=burger>
+        <img>
+</div>
+</div>
+    
+</body>
+</html>
