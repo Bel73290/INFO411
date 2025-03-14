87ae1aade05b711e4476c9f64a3adb1010e681e3
Ilias Bellir <bellirilias@gmail.com>
2025-03-14 14:31:24 +0100

    le goat ma aider

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
-
 <!doctype html>
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title>Le Ciel burgouzz</title>
+  <title>Le Ciel Burgouzz</title>
   <link rel="stylesheet" href="style.css">
   <script src="script.js"></script>
 </head>
 <body>
 <div>
     <h1>Le Ciel Burgouzz</h1>
-    <a>Connexion</a>
+    <a href="#">Connexion</a> <!-- Ajout d'un href pour rendre le lien cliquable -->
 </div>
 <div>
     <h2>Menu</h2>
-    <div class=burger>
+    <div class="burger"> <!-- Ajout des guillemets pour la classe CSS -->
         <?php
-        for ($i=0 ; $i>10; $i++){
+        for ($i = 0; $i < 10; $i++) {
             echo "yoyo ";
         }
         ?>
         <button>Choisir</button>
-</div>
+    </div>
 </div>
     
 </body>
