7c340eda9a14d69e66989804033a9df2aa2d9572
BRAVET <antobravet@gmail.com>
2025-03-14 14:50:11 +0100

    retirer les description

--- index.php
+++ index.php
@@ -14,37 +14,31 @@
         <div class="menu-item">
             <img src="Classic Burgouzz.png" alt="Classic Burgouzz">
             <h2>Classic Burgouzz</h2>
-            <p>Smash burger de viande française et cheddar mature de Somerset, sauce burgouzz avec salade, cornichons et pain bun brioché.</p>
         </div>
 
         <div class="menu-item">
             <img src="Green Burgouzz.png" alt="Green Burgouzz">
             <h2>Green Burgouzz</h2>
-            <p>Un steak d'haubergine mariné et cheddar mature de Somerset, spicy mayo, salade, kimchi dans un pain brioché.</p>
         </div>
 
         <div class="menu-item">
             <img src="Kaarage Burgouzz.png" alt="Kaarage Burgouzz">
             <h2>Kaarage Burgouzz</h2>
-            <p>Poulet kaarage mariné et cheddar mature de Somerset avec une sauce mayonnaise sriracha, salade, kimchi et bun brioché.</p>
         </div>
 
         <div class="menu-item">
             <img src="Summer Burgouzz.png" alt="Summer Burgouzz">
             <h2>Summer Burgouzz</h2>
-            <p>Un steak smashé de viande française et cheddar mature de Somerset, guacamole au kiwi, salade, oignons rouges et bun brioché.</p>
         </div>
 
         <div class="menu-item">
             <img src="Truffle Burgouzz.png" alt="Truffle Burgouzz">
             <h2>Truffle Burgouzz</h2>
-            <p>Smash burger de viande française, cheddar mature, tapenade de truffes noires du Périgord et bun brioché.</p>
         </div>
 
         <div class="menu-item">
             <img src="Winter Burgouzz.png" alt="Winter Burgouzz">
             <h2>Winter Burgouzz</h2>
-            <p>Deux steaks smashés, fromage à raclette fumé, mayonnaise miel et moutarde, salade, compotée d’oignons et de marrons, bun bacon de dinde et épices de Noël.</p>
         </div>
     </div>
 
