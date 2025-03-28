3efdee9a1881f912f7d6fbef1cdcfc336122af92
Ilias Bellir <bellirilias@gmail.com>
2025-03-28 10:02:43 +0100

    ee

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-	    define ('SERVEUR_BD','localhost');
+	    define ('SERVEUR_BD','c_mariadb:3306');
 	    define ('LOGIN_BD','name_burger');
 	    define ('PASS_BD','rootpassword');
 	    define ('NOM_BD','burger');
