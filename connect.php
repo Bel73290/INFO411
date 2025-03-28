026b95e01ba12561aa2fc29a6e35da2b085abf53
Ilias Bellir <bellirilias@gmail.com>
2025-03-28 09:41:49 +0100

    correction connect

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 	    define ('SERVEUR_BD','localhost');
-	    define ('LOGIN_BD','root');
+	    define ('LOGIN_BD','name_burger');
 	    define ('PASS_BD','rootpassword');
 	    define ('NOM_BD','burger');
 ?>
